424fecc4d6f30b39877de0deb666b6921ac3fd21
Night Skies <2900912841@qq.com>
2025-11-23 16:36:56 +0800

    Add basic array operations using NumPy

---
+++ numpy/basic_array.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+a = np.array([1, 2, 3, 4])
+b = np.array([10, 20, 30, 40])
+
+print("a:", a)
+print("b:", b)
+print("a + b:", a + b)
+print("a * 2:", a * 2)
+print("a 的均值:", a.mean())
